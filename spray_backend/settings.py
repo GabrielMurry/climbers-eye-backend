8b6d5a9ad00830fe77c71fa6106a0dd0207b4c45
Gabriel <gabrielmurryedh@gmail.com>
2023-10-27 19:44:06 -0700

    testing middleware issue part 4

--- spray_backend/settings.py
+++ spray_backend/settings.py
@@ -61,8 +61,8 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.contrib.messages.middleware.MessageMiddleware',
+    # 'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     # 'corsheaders.middleware.CorsMiddleware',
 ]
